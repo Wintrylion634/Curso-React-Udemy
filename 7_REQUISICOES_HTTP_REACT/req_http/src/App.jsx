7a337fa3c9874f6ed1c7f9aef18033354bcf870d
Wintrylion634 <nathan_vieira.azevedo@hotmail.com>
2025-11-24 12:44:37 -0300

    feat: add estados de loading, delete de produtos e tratamento de erros

--- 7_REQUISICOES_HTTP_REACT/req_http/src/App.jsx
+++ 7_REQUISICOES_HTTP_REACT/req_http/src/App.jsx
@@ -1,17 +1,107 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import { useFetch } from "./hooks/useFetch";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // 1 criação de link com a API e fetch do data (getproducts)
+
+  const [products, setProducts] = useState([]);
+
+  const url = "http://localhost:3000/products";
+
+  //4 - custom hook
+
+  const { data: items, httpConfig, loading, error } = useFetch(url);
+
+  /* useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+
+    fetchData();
+  }, []); */
+
+  //2 - Add products
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+
+    httpConfig(product, "POST");
+
+    setName("");
+    setPrice("");
+  };
+
+  const deleteProduct = async (id) => {
+    httpConfig(id, "DELETE");
+  };
 
   return (
     <>
-      <div className='app'>
-        <h1>Requisições HTTPS React</h1>
-        <p>Teste</p>
+      <div className="app">
+        <h1>Lista de Produtos</h1>
+        {/*render condicional loading*/}
+        {loading && <p className="loading-text"> Carregando Produtos....</p>}
+        {error && <p className="error-text">{error}</p>}
+        {!loading && !error && (
+          <div className="container-produtos">
+            <ul>
+              {items &&
+                items.map((product) => (
+                  <li className="product" key={product.id}>
+                    {product.name} - R$ {product.price}
+                    <button
+                      className="btn-delete-produto"
+                      onClick={() => deleteProduct(product.id)}
+                    >
+                      Excluir
+                    </button>
+                  </li>
+                ))}
+            </ul>
+          </div>
+        )}
+
+        <div className="add-product">
+          <form onSubmit={handleSubmit}>
+            <label>Nome:</label>
+            <input
+              type="text"
+              name="nome"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+            <label>Preço:</label>
+            <input
+              type="number"
+              name="price"
+              value={price}
+              onChange={(e) => {
+                setPrice(e.target.value);
+              }}
+            />
+            {/* 7- Loading para o post*/}
+            {loading && <input type="submit" disabled value="Aguarde" />}
+            {!loading && <input type="submit" value="Criar" />}
+          </form>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
